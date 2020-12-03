75e6cb7b1728a0ab8b212f41aab0267b2035bb0e
JMTeixeira17 <jmteixeira.17@est.ucab.edu.ve>
2020-12-03 10:21:59 -0400

    Modificacion de Archivo: app-routing

--- mercadeo-frontend/src/app/app-routing.module.ts
+++ mercadeo-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {Route} from '@angular/router';
+import {ModuleWithProviders} from '@angular/core';
+
+//Imports de los componentes.
+
+import { LoginComponent } from './components/login/login.component';
+
+//Rutas de la app
+const routes: Routes = [
+  {path: 'login',component: LoginComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<Route> = RouterModule.forRoot(routes);
