1816687e8a999313d994de4c2cee33efceeb3a33
aandradel19 <aandradel.17@est.ucab.edu.ve>
2020-12-06 17:44:24 -0400

    Tarea-017

---
+++ mercadeo-backend/servicio/src/test/java/Pregunta_estudioORMWS_Test.java
@@ -0,0 +1,53 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import ucab.dsw.dtos.*;
+import ucab.dsw.entidades.Pregunta_estudio;
+
+import java.util.List;
+
+public class Pregunta_estudioORMWS_Test {
+
+    @Test
+    public void addPregunta_estudioTest() throws Exception
+    {
+        ucab.dsw.servicio.Pregunta_estudioORMWS servicio = new ucab.dsw.servicio.Pregunta_estudioORMWS();
+        Pregunta_estudioDto pregunta_estudioDto = new Pregunta_estudioDto();
+        pregunta_estudioDto.setEstado( "A" );
+        EstudioDto estudio = new EstudioDto( 1);
+        pregunta_estudioDto.setEstudioDto( estudio );
+        Pregunta_encuestaDto pregunta_encuesta = new Pregunta_encuestaDto( 1);
+        pregunta_estudioDto.setPreguntaEncuestaDto( pregunta_encuesta );
+        Pregunta_estudioDto resultado = servicio.addPregunta_estudio( pregunta_estudioDto );
+        Assert.assertNotEquals( resultado.getId(), 0  );
+    }
+
+    @Test
+    public void deletePregunta_estudioTest() throws Exception{
+        ucab.dsw.servicio.Pregunta_estudioORMWS servicio = new ucab.dsw.servicio.Pregunta_estudioORMWS();
+        Pregunta_estudioDto resultado = servicio.deletePregunta_estudio(1);
+        Assert.assertNotEquals( resultado.getId(), 0  );
+    }
+
+    @Test
+    public void showPregunta_estudiosTest() throws Exception{
+        ucab.dsw.servicio.Pregunta_estudioORMWS servicio = new ucab.dsw.servicio.Pregunta_estudioORMWS();
+        List<Pregunta_estudio> resultado = servicio.showPregunta_estudios();
+        Assert.assertNotEquals(resultado, null);
+    }
+
+    @Test
+    public void updatePregunta_estudioTest() throws Exception
+    {
+        ucab.dsw.servicio.Pregunta_estudioORMWS servicio = new ucab.dsw.servicio.Pregunta_estudioORMWS();
+        Pregunta_estudioDto pregunta_estudioDto = new Pregunta_estudioDto();
+        pregunta_estudioDto.setEstado( "I" );
+        EstudioDto estudio = new EstudioDto( 2);
+        pregunta_estudioDto.setEstudioDto( estudio );
+        Pregunta_encuestaDto pregunta_encuesta = new Pregunta_encuestaDto( 2);
+        pregunta_estudioDto.setPreguntaEncuestaDto( pregunta_encuesta );
+        Pregunta_estudioDto resultado = servicio.updatePregunta_estudio( 1, pregunta_estudioDto );
+        Assert.assertNotEquals( resultado.getId(), 0  );
+    }
+}
