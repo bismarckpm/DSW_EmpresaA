ef0dd6b533a82dff39b6cca8bad235e0beac690b
JMTeixeira17 <jmteixeira.17@est.ucab.edu.ve>
2020-12-08 00:36:28 -0400

    Modulo preguntas

--- mercadeo-frontend/src/app/app-routing.module.ts
+++ mercadeo-frontend/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {Route} from '@angular/router';
+import {ModuleWithProviders} from '@angular/core';
+
+//Imports de los componentes.
+
+import { LoginComponent } from './components/login/login.component';
+import { RegistraPreguntaComponent } from './components/pregunta/registra-pregunta/registra-pregunta.component';
+import { ConsultaPreguntaComponent } from './components/pregunta/consulta-pregunta/consulta-pregunta.component';
+
+//Rutas de la app
+const routes: Routes = [
+  {path: 'login',component: LoginComponent},
+  {path: 'registraPregunta', component: RegistraPreguntaComponent},
+  {path: 'listadoPregunta',component: ConsultaPreguntaComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders<Route> = RouterModule.forRoot(routes);
--- mercadeo-frontend/src/app/app.module.ts
+++ mercadeo-frontend/src/app/app.module.ts
@@ -1,21 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { routing, appRoutingProviders } from './app-routing.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatIconModule} from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+import {MatButtonModule} from '@angular/material/button';
 
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistraPreguntaComponent } from './components/pregunta/registra-pregunta/registra-pregunta.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { ConsultaPreguntaComponent } from './components/pregunta/consulta-pregunta/consulta-pregunta.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    RegistraPreguntaComponent,
+    NavigationComponent,
+    ConsultaPreguntaComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    routing,
+    NoopAnimationsModule,
+    MatSliderModule,
+    MatIconModule,
+    CommonModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders,
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
