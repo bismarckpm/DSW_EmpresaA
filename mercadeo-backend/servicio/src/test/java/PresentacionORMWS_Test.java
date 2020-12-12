f7a94d4a951934d98ee13d95f63685fdb7cc1389
unknown <gli.16@est.ucab.edu.ve>
2020-12-12 01:38:20 -0400

    conflictos merge con tarea-020

--- mercadeo-backend/servicio/src/test/java/PresentacionORMWS_Test.java
+++ mercadeo-backend/servicio/src/test/java/PresentacionORMWS_Test.java
@@ -44,10 +44,7 @@ public class PresentacionORMWS_Test {
         presentacionDto.setTitulo( "TituloModif" );
         presentacionDto.setCaracteristicas( "CaracaModif" );
         presentacionDto.setEstado( "I" );
-<<<<<<< HEAD
-=======
         ProductoDto producto = new ProductoDto( 2);
->>>>>>> 50c9f9f1154c01d68d1ab5f3532d72268b51fecc
         PresentacionDto resultado = servicio.updatePresentacion( 1, presentacionDto );
         Assert.assertNotEquals( resultado.getId(), 0  );
     }
