7fd8a414f89a64091abd3c929c01f8a4258dc6f8
GerLC <gli.16@est.ucab.edu.ve>
2020-12-18 05:46:00 -0400

    roles

--- mercadeo-frontend/src/app/components/sidebar2/sidebar2.component.ts
+++ mercadeo-frontend/src/app/components/sidebar2/sidebar2.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/modelos/user';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-sidebar2',
@@ -7,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Sidebar2Component implements OnInit {
   
-  constructor() { }
+  constructor( private _loginService: LoginService) { }
+
+  // Usuarios
+  public identity: any;
+  isUser = false;
+  public user: User = {
+    id:0,
+    nombreUsuario:'',
+    correo:'',
+    estado:'',
+    idRol:0
+  };
 
   ngOnInit(): void {
-    
+    this.getUser();
+  }
+
+  getUser(): void {
+    this.identity = JSON.parse(this._loginService.getIdentity());
+    this.user = new User(
+      this.identity.id,
+      this.identity.nombreUsuario,
+      this.identity.correo,
+      this.identity.estado,
+      this.identity.idRol )
+      if (this.user) {
+        this.isUser = true;
+      }
+      console.log(this.user)
   }
 
 
