34684d45a2b90b44f237702b2db55ceca123d619
JMTeixeira17 <jmteixeira.17@est.ucab.edu.ve>
2020-12-26 10:30:10 -0400

    Añadiendo pregunta dinámico

--- mercadeo-frontend/src/app/components/admin/admin_pregunta/registra-pregunta/registra-pregunta.component.ts
+++ mercadeo-frontend/src/app/components/admin/admin_pregunta/registra-pregunta/registra-pregunta.component.ts
@@ -12,6 +12,8 @@ import { SubcategoriaService } from 'src/app/services/subcategoria.service';
 import { FormArray, FormBuilder, Validators } from '@angular/forms';
 import { LoginService } from 'src/app/services/login.service';
 import { User } from 'src/app/interfaces/user';
+import { Respuesta_Pregunta } from 'src/app/interfaces/respuesta_pregunta';
+import { RespuestapreguntaService } from 'src/app/services/respuestapregunta.service';
 
 @Component({
   selector: 'app-registra-pregunta',
@@ -30,6 +32,13 @@ export class RegistraPreguntaComponent implements OnInit {
      subcategoriaDto: 0,
      usuarioDto: 0
     };
+
+    respuesta_pregunta: Respuesta_Pregunta = {
+      id: 0,
+      nombre: '',
+      estado: '',
+      preguntaEncuestaDto: 0
+    }
     
   public identity;
   public user: User;
@@ -52,6 +61,7 @@ export class RegistraPreguntaComponent implements OnInit {
 
   constructor(
     private _preguntaService: PreguntaService,
+    private _respuestaPreguntaService: RespuestapreguntaService,
     private _subcategoriaService: SubcategoriaService,
     public _http: HttpClient,
     private _router: Router,
@@ -107,11 +117,16 @@ añadeRespuesta() {
     id: 0,
     nombre: [""],
     estado: ["A"],
+    preguntaEncuestaDto: 0
   })
 }
 
 addNext() {
   (this.registrarPreguntaForm.controls['respuestaAsignada'] as FormArray).push(this.añadeRespuesta());
+}
+
+eliminarOpcion(index: number) {
+  (this.registrarPreguntaForm.controls['respuestaAsignada'] as FormArray).removeAt(index);
 }
 
 
@@ -134,5 +149,34 @@ addNext() {
 
   }
 
+  creaPregunta(){
 
+    this.pregunta_encuesta = {
+      id: 0,
+      descripcion: this.registrarPreguntaForm.get("descripcion").value,
+      tipoPregunta: this.registrarPreguntaForm.get("tipoPregunta").value,
+      estado: "A",
+      subcategoriaDto: this.registrarPreguntaForm.get("subcategoriaDto").value._id,
+      usuarioDto: this.user.id
+    }
+
+    const respuestas = this.registrarPreguntaForm.get("respuestaAsignada").value;
+
+    console.log(this.pregunta_encuesta);
+    console.log(respuestas);
+
+    this._preguntaService.registrarPregunta(this.pregunta_encuesta).subscribe(
+      response =>  {
+        console.log(response);
+        if(this.pregunta_encuesta.tipoPregunta == 'Seleccion simple' || this.pregunta_encuesta.tipoPregunta == 'Seleccion multiple' ){
+          this._respuestaPreguntaService.registraRespuestaConPregunta(respuestas,response.id).subscribe(
+            respuesta => {
+              console.log(respuesta);
+            }
+          );
+        }
+      }
+    );
+    //this._router.navigate(['/listadoPregunta']);
+    }
 }
