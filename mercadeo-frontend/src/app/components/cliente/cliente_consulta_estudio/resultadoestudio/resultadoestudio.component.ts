904e5b2452ed71d08cb467fc323f596161309ee2
GerLC <gli.16@est.ucab.edu.ve>
2020-12-31 14:10:20 -0400

    3dchart

--- mercadeo-frontend/src/app/components/cliente/cliente_consulta_estudio/resultadoestudio/resultadoestudio.component.ts
+++ mercadeo-frontend/src/app/components/cliente/cliente_consulta_estudio/resultadoestudio/resultadoestudio.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { EstudioclienteService } from '../../../../services/estudiocliente.service';
 import { PreguntaService } from '../../../../services/pregunta.service';
 import { Estudio } from '../../../../interfaces/estudio';
@@ -11,7 +11,14 @@ import { Usuario } from '../../../../interfaces/usuario';
 import { Solicitud_Estudio } from 'src/app/interfaces/solicitud_estudio';
 import { ActivatedRoute } from '@angular/router';
 import { Categoria } from '../../../../interfaces/categoria';
+
+import { MatPaginator } from '@angular/material/paginator';
+
 import * as Highcharts from 'highcharts';
+
+import highcharts3D from 'highcharts/highcharts-3d';
+
+highcharts3D(Highcharts);
 
 @Component({
   selector: 'app-resultadoestudio',
@@ -21,6 +28,9 @@ import * as Highcharts from 'highcharts';
 })
 export class ResultadoestudioComponent implements OnInit {
 
+  // Paginator
+  @ViewChild(MatPaginator, { static: false })
+  paginator!: MatPaginator;
 
   public idEstudio:any;
   estudio: any = [];
@@ -40,14 +50,45 @@ export class ResultadoestudioComponent implements OnInit {
   }
 
 // HighCharts
-
+// Funcion para crear el highchart
+// El enunciado recibe el titulo o la pregunta
+// Valor obtendra 'name' e 'y' como los datos para generar la grafica
 chart(enunciado: any, valor: any): Highcharts.Options {
 
   let chartOptions: Highcharts.Options = {
 
+    chart: {
+        type: "pie",
+        plotShadow: false,
+        options3d: {
+          enabled: true,
+          alpha: 45,
+          beta: 0,
+      },
+    },
     title: {
         text: enunciado
     },
+    tooltip: {
+      headerFormat: "",
+      pointFormat:
+        "<span style='color:{point.color}'>\u25CF</span> {point.name}: <b>{point.y}</b>",
+      style: {
+        fontSize: '10px'
+      },
+    },
+    plotOptions : {
+      pie: {
+         allowPointSelect: true,
+         cursor: 'pointer',
+   
+         dataLabels: {
+            enabled: false           
+         },
+   
+         showInLegend: true
+      }
+   },
     series: [
       {
         type: "pie",
@@ -80,14 +121,14 @@ chartOptions: Highcharts.Options[] = [];
      }
    );
      console.log(this.idEstudio);
-     this.resultadoEstudio(this.idEstudio.estudio);
+     this.resultadoEstudio(1);
 
   }
 
 resultadoEstudio(idEstudio: number){
     this._EstudioclienteService.resultadoEstudio(idEstudio).subscribe(
       (response) => {
-        this.estudio = response[1]._listaRespuestas;
+        this.estudio = response[0]._listaRespuestas;
         this.nombre = response;
         console.log(this.estudio);
         console.log(this.nombre);
@@ -95,12 +136,11 @@ resultadoEstudio(idEstudio: number){
 
 
         // const enunciado = response[0]._enunciado;
-          // const valor = this.estudio.map((x:any) => { return {name: x._descripcion, y: x._valor} })
+        // const valor = this.estudio.map((x:any) => { return {name: x._descripcion, y: x._valor} })
         // console.log(enunciado);
         // console.log(valor);
 
         this.nombre.forEach(element => {
-          //this.estudio = element._listaRespuestas[0];
 
           if(element._tipoPregunta != 'Abierta'){
           const valor = element._listaRespuestas.map((x:any) => { return {name: x._descripcion, y: x._valor} })
@@ -111,11 +151,16 @@ resultadoEstudio(idEstudio: number){
           console.log('valor', valor);
 
           this.chartOptions.push( this.chart(enunciado, valor ) );
-          }else{
-             this.respuestaAbierta = element._enunciado;
+          } else{
+
+            this.respuestaAbierta = element._enunciado;
             this.respAbierta = element._listaRespuestas.map((x:any) => { return {name: x._descripcion, y: x._valor} })
             console.log('abierta', this.respAbierta);
             console.log(this.respAbierta);
+
+            this.respAbierta.paginator = this.paginator;
+            this.respuestaAbierta.paginator = this.paginator;
+
           }
 
 
