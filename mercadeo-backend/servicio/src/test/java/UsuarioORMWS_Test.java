77e1603c6b312908564b402142c4746f93f4ae66
raikjars <raikjars@gmail.com>
2021-01-05 04:53:49 -0400

    Mail configurado

--- mercadeo-backend/servicio/src/test/java/UsuarioORMWS_Test.java
+++ mercadeo-backend/servicio/src/test/java/UsuarioORMWS_Test.java
@@ -2,6 +2,8 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import ucab.dsw.Response.ListaEncuestasE;
+import ucab.dsw.accesodatos.DaoDato_usuario;
+import ucab.dsw.accesodatos.DaoRol;
 import ucab.dsw.dtos.Dato_usuarioDto;
 import ucab.dsw.dtos.LoginDto;
 import ucab.dsw.dtos.RolDto;
@@ -51,6 +53,7 @@ public class UsuarioORMWS_Test {
 
     }
 
+
     @Test
     public void deleteTest() throws Exception {
 
@@ -86,4 +89,39 @@ public class UsuarioORMWS_Test {
         List<ListaEncuestasE> resultado = servicio.dashboardEncuestado(1);
         Assert.assertNotEquals(resultado, null);
     }
+
+    @Test
+    public void create30UsuariosTest() throws Exception {
+
+        int cont = 1;
+        long val =0;
+        long du = 1;
+        DaoRol daoRol = new DaoRol();
+        DaoDato_usuario daoDu = new DaoDato_usuario();
+        for (cont = 1; cont <= 30; cont++){
+            UsuarioDto usuario = new UsuarioDto();
+            val++;
+            Rol rol = daoRol.find(val, Rol.class);
+            RolDto rolDto = new RolDto(rol.get_id());
+            if (val == 4){
+                Dato_usuario datoUsuario = daoDu.find(du, Dato_usuario.class);
+                Dato_usuarioDto duDto = new Dato_usuarioDto(datoUsuario.get_id());
+                usuario.setDatoUsuarioDto(duDto);
+                du++;
+                val=0;
+            }
+            else{
+                Dato_usuarioDto datoUsuario = null;
+                usuario.setDatoUsuarioDto(datoUsuario);
+            }
+            usuario.setRolDto(rolDto);
+            usuario.setPassword("1234");
+            usuario.setCorreo("prueba" + cont + "@gmail.com");
+            usuario.setNombreUsuario("Usuario" + cont);
+            Assert.assertEquals(usuario.getCorreo(), servicio.create(usuario).get_correo());
+        }
+
+    }
+
+
 }
