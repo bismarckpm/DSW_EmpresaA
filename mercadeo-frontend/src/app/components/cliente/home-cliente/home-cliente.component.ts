ad86457c84e9d1e0a824709e9841fef7d7d191c2
GerLC <gli.16@est.ucab.edu.ve>
2021-02-02 13:39:55 -0400

    estilos

--- mercadeo-frontend/src/app/components/cliente/home-cliente/home-cliente.component.ts
+++ mercadeo-frontend/src/app/components/cliente/home-cliente/home-cliente.component.ts
@@ -139,7 +139,7 @@ export class HomeClienteComponent implements OnInit {
       this.estudios = response;
       console.log('before', this.estudios);
       this.estudios = this.estudios.filter(item => item._idSolicitudEstudio._id == idSolicitud);
-      this.estudios = this.estudios.filter(item => item._idSolicitudEstudio._id == idSolicitud);
+     
       console.log('after', this.estudios);
     })
   }
