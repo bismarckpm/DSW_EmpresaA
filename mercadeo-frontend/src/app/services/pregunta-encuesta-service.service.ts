d1aaf89703b29adeef5e4bb93c6d42276efd2413
GerLC <gli.16@est.ucab.edu.ve>
2021-02-14 02:30:47 -0400

    fix encuestado null validarEstudios

--- mercadeo-frontend/src/app/services/pregunta-encuesta-service.service.ts
+++ mercadeo-frontend/src/app/services/pregunta-encuesta-service.service.ts
@@ -1,11 +1,16 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { delay, map, retry, retryWhen, take } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PreguntaEncuestaServiceService {
+  
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(private httpClient: HttpClient) { }
 
@@ -25,9 +30,23 @@ export class PreguntaEncuestaServiceService {
     return this.httpClient.get(`http://localhost:8080/mercadeo-backend/api/pregunta_estudio/preguntasRecomendadas/${id}`);
   }
 
+  // validarPreguntas(idE: number, idU: number): Observable<any> {
+  //   return this.httpClient.get(`http://localhost:8080/mercadeo-backend/api/respuesta/validarEstatus/${idE}/${idU}`, this.httpOptions
+  //     )
+  // }
+  //  { responseType: 'text'}
+
+
   validarPreguntas(idE: number, idU: number): Observable<any> {
-    return this.httpClient.get(`http://localhost:8080/mercadeo-backend/api/respuesta/validarEstatus/${idE}/${idU}`,
-    //  { responseType: 'text'}
-      );
+    return this.httpClient.get(`http://localhost:8080/mercadeo-backend/api/respuesta/validarEstatus/${idE}/${idU}`, this.httpOptions
+      ).pipe(
+        map((response:any) => {
+          if (response.objeto == null) {
+            throw new Error(); // Will be caught by `map` and reemitted as an error notification.
+          }
+          return response;
+        }),
+        retryWhen(errors => errors.pipe(take(2), delay(1000))),
+      )
   }
 }
